52c5698afba22117179e5bcf2cc20e541f6a0b38
Matthew Tao <s134584@gapps.dilworth.school.nz>
2020-08-03 07:58:52 +0000

    remove loot being added twice fix not being able to exit inventory combine item selections into one if statement

--- main.py
+++ main.py
@@ -81,8 +81,7 @@ def battle(combat_monster):
 		color_print(Fore.GREEN, f"You have defeated {combat_monster.name}")
 		player.xp[0] += combat_monster.xp
 		player.xp_check()
-		for i in range (random.randint(0, 3)):
-			player.inventory.append(Equipment("Iron", "Sword"))
+		player.inventory.append(Equipment("Iron", "Sword"))
 	elif player.health[0] < 0:
 		color_print(Fore.RED, f"{combat_monster.name} has defeated you")
 
@@ -139,108 +138,108 @@ class Equipment:
 
 
 class Character:
-    def __init__(self):
-        self.name = input("what is your name? \n")
-        system("clear")
-        self.health = [10, 10]
-        self.mana = [5, 5]
-        self.attack = 3
-        self.defence = 0
-        self.xp = [0, 50]
-        self.level = 1
-        self.inventory = []
+	def __init__(self):
+		self.name = input("what is your name? \n")
+		system("clear")
+		self.health = [10, 10]
+		self.mana = [5, 5]
+		self.attack = 3
+		self.defence = 0
+		self.xp = [0, 50]
+		self.level = 1
+		self.inventory = []
 
-    def stats(self):
-        self.attributes = vars(self)
-        self.attributes_keys = list(self.attributes.keys())
-        self.attributes_keys.pop()
-        self.attributes_keys.pop()
-        print(f"{Fore.GREEN}STATS:")
-        for attribute in self.attributes_keys:
-            print(
-                f"{(attribute.replace('_', ' ').upper())}: {self.attributes.get(attribute)}"
-            )
-        del self.attributes
-        del self.attributes_keys
-        print(Style.RESET_ALL)
+	def stats(self):
+		self.attributes = vars(self)
+		self.attributes_keys = list(self.attributes.keys())
+		self.attributes_keys.pop()
+		self.attributes_keys.pop()
+		print(f"{Fore.GREEN}STATS:")
+		for attribute in self.attributes_keys:
+			print(
+				f"{(attribute.replace('_', ' ').upper())}: {self.attributes.get(attribute)}"
+			)
+		del self.attributes
+		del self.attributes_keys
+		print(Style.RESET_ALL)
 
-    def level_up(self):
-        points = 3
-        while points > 0:
-            allocate = int(input("Allocate points:\n"
-                           "-1- HEALTH\n"
-                           "-2- MANA\n"
-                           "-3- ATTACK\n"
-                           "-4- DEFENCE\n"))
-            if allocate == 1:
-                self.health[1] += 1
-            elif allocate == 2:
-                self.mana[1] += 1
-            elif allocate == 3:
-                self.attack += 1
-            elif allocate == 4:
-                self.defence += 1
-            points -= 1
+	def level_up(self):
+		points = 3
+		while points > 0:
+			allocate = int(input("Allocate points:\n"
+							"-1- HEALTH\n"
+							"-2- MANA\n"
+							"-3- ATTACK\n"
+							"-4- DEFENCE\n"))
+			if allocate == 1:
+				self.health[1] += 1
+			elif allocate == 2:
+				self.mana[1] += 1
+			elif allocate == 3:
+				self.attack += 1
+			elif allocate == 4:
+				self.defence += 1
+			points -= 1
 
 
-    def xp_check(self):
-        if self.xp[0] > self.xp[1]:
-            self.level += 1
-            self.xp[0] -= self.xp[1]
-            # XP to next level follows a logarithmic curve base 10 multiplier.
-            self.xp[1] = round((math.log(self.level, 10) + 1) * self.xp[1], 0)
-            color_print(Fore.GREEN, "You've leveled up!")
-            self.level_up()
+	def xp_check(self):
+		if self.xp[0] > self.xp[1]:
+			self.level += 1
+			self.xp[0] -= self.xp[1]
+			# XP to next level follows a logarithmic curve base 10 multiplier.
+			self.xp[1] = round((math.log(self.level, 10) + 1) * self.xp[1], 0)
+			color_print(Fore.GREEN, "You've leveled up!")
+			self.level_up()
 
-    def show_inventory(self):
-        print("INVENTORY:")
-        inventory_names = []
-        for item in self.inventory:
-            inventory_names.append(item.name)
-        print(inventory_names)
-        while True:
-            try:
-                select_item = int(
-                    input("What would you like to do? \n"
-                          "-1- Re-roll modifier\n"
-                          "-2- Equip item\n"
-                          "-3- Exit\n"))
-                if select_item == 1:
-                    remove_choice = int(input("Which item?\n")) - 1
-                    if self.inventory[remove_choice].isEquipped:
-                        color_print(Fore.RED, "Can't re-roll while equipped")
-                    else:
-                        self.inventory[remove_choice].roll_mod()
-                        self.inventory[remove_choice].stats()
-                        color_print(Fore.GREEN, "Mod re-rolled\n")
-                elif select_item == 2:
-                    equip_choice = int(input("Which item\n")) - 1
-                    self.equip_item(self.inventory[equip_choice])
-                elif select_item == 3:
-                    system("clear")
-                    break
-                else:
-                    color_print(Fore.RED, "1that's not a valid number!\n")
-            except:
-                color_print(Fore.RED, "2that's not a valid number!\n")
+	def show_inventory(self):
+		print("INVENTORY:")
+		inventory_names = []
+		for item in self.inventory:
+			inventory_names.append(item.name)
+		print(inventory_names)
+		while True:
+			try:
+				inventory_action = int(
+					input("What would you like to do? \n"
+							"-1- Re-roll modifier\n"
+							"-2- Equip item\n"
+							"-3- Exit\n"))
+				if inventory_action == 1 or inventory_action == 2:
+					select_item = int(input("Which item?\n")) - 1
+				if inventory_action == 1:
+					if self.inventory[select_item].isEquipped:
+						color_print(Fore.RED, "Can't re-roll while equipped")
+					else:
+						self.inventory[select_item].roll_mod()
+						self.inventory[select_item].stats()
+						color_print(Fore.GREEN, "Mod re-rolled\n")
+				elif inventory_action == 2:
+					self.equip_item(self.inventory[select_item])
+				elif inventory_action == 3:
+					system("clear")
+					break
+				else:
+					color_print(Fore.RED, "1that's not a valid number!\n")
+			except:
+				color_print(Fore.RED, "2that's not a valid number!\n")
 
-    def equip_item(self, item):
-        if item.isEquipped:
-            self.health[1] -= item.health
-            self.mana[1] -= item.mana
-            self.attack -= item.attack
-            self.defence -= item.defence
-            item.isEquipped = False
-            print(f"You have unequipped {item.name}")
-        else:
-            self.health[0] += item.health
-            self.health[1] += item.health
-            self.mana[0] += item.mana
-            self.mana[1] += item.mana
-            self.attack += item.attack
-            self.defence += item.defence
-            item.isEquipped = True
-            print(f"You have equipped {item.name}")
+	def equip_item(self, item):
+		if item.isEquipped:
+			self.health[1] -= item.health
+			self.mana[1] -= item.mana
+			self.attack -= item.attack
+			self.defence -= item.defence
+			item.isEquipped = False
+			print(f"You have unequipped {item.name}")
+		else:
+			self.health[0] += item.health
+			self.health[1] += item.health
+			self.mana[0] += item.mana
+			self.mana[1] += item.mana
+			self.attack += item.attack
+			self.defence += item.defence
+			item.isEquipped = True
+			print(f"You have equipped {item.name}")
 
 
 class Monster:

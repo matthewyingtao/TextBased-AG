4e74ec91be0f5b7e7a9e9c355156c72fc593e6c0
Ash Schmutz <s164874@gapps.dilworth.school.nz>
2020-08-04 20:57:44 +0000

    inventory numbers

--- main.py
+++ main.py
@@ -125,7 +125,6 @@ class Equipment:
 		self.isEquipped = False
 
 	# remove mod from an item's stats
-
 	def remove_mod(self):
 		self.name = self.name[len(self.mod) + 1:]
 		self.health -= self.mod_effect[0]
@@ -136,7 +135,6 @@ class Equipment:
 		self.mod_effect = [0, 0, 0, 0]
 
 	# roll new mod on item
-
 	def roll_mod(self):
 		self.remove_mod()
 		self.mod = implicits_list[random.randint(0, len(implicits_list) - 1)]
@@ -162,105 +160,104 @@ class Equipment:
 
 
 class Character:
-	def __init__(self):
-		self.name = input("what is your name? \n")
-		system("clear")
-		self.health = [10, 10]
-		self.mana = [5, 5]
-		self.attack = 3
-		self.defence = 0
-		self.xp = [0, 50]
-		self.level = 1
-		self.inventory = []
+  def __init__(self):
+    self.name = input("what is your name? \n")
+    system("clear")
+    self.health = [10, 10]
+    self.mana = [5, 5]
+    self.attack = 3
+    self.defence = 0
+    self.xp = [0, 50]
+    self.level = 1
+    self.inventory = []
 
-	def stats(self):
-		self.attributes = vars(self)
-		self.attributes_keys = list(self.attributes.keys())
-		self.attributes_keys.pop()
-		self.attributes_keys.pop()
-		print(f"{Fore.GREEN}STATS:")
-		for attribute in self.attributes_keys:
-			print(
-				f"{(attribute.replace('_', ' ').upper())}: {self.attributes.get(attribute)}"
-			)
-		del self.attributes
-		del self.attributes_keys
-		print(Style.RESET_ALL)
+  def stats(self):
+    self.attributes = vars(self)
+    self.attributes_keys = list(self.attributes.keys())
+    self.attributes_keys.pop()
+    self.attributes_keys.pop()
+    print(f"{Fore.GREEN}STATS:")
+    for attribute in self.attributes_keys:
+      print(
+        f"{(attribute.replace('_', ' ').upper())}: {self.attributes.get(attribute)}"
+      )
+    del self.attributes
+    del self.attributes_keys
+    print(Style.RESET_ALL)
 
-	# allocation of stat points
-	def level_up(self):
-		points = 3
-		while points > 0:
-			allocate = input_handler(
-				1, 4, "Allocate points:\n"
-				"-1- HEALTH\n"
-				"-2- MANA\n"
-				"-3- ATTACK\n"
-				"-4- DEFENCE\n")
-			if allocate == 1:
-				self.health[1] += 1
-			elif allocate == 2:
-				self.mana[1] += 1
-			elif allocate == 3:
-				self.attack += 1
-			elif allocate == 4:
-				self.defence += 1
-			points -= 1
+  # allocation of stat points
+  def level_up(self):
+    points = 3
+    while points > 0:
+      allocate = input_handler(
+        1, 4, "Allocate points:\n"
+        "-1- HEALTH\n"
+        "-2- MANA\n"
+        "-3- ATTACK\n"
+        "-4- DEFENCE\n")
+      if allocate == 1:
+        self.health[1] += 1
+      elif allocate == 2:
+        self.mana[1] += 1
+      elif allocate == 3:
+        self.attack += 1
+      elif allocate == 4:
+        self.defence += 1
+      points -= 1
 
-	# check xp after battle, and increases the xp requirement for the next level
-	def xp_check(self):
-		if self.xp[0] > self.xp[1]:
-			self.level += 1
-			self.xp[0] -= self.xp[1]
-			# XP to next level follows a logarithmic curve base 10 multiplier.
-			self.xp[1] = round((math.log(self.level, 10) + 1) * self.xp[1], 0)
-			color_print(Fore.GREEN, "You've leveled up!")
-			self.level_up()
+  # check xp after battle, and increases the xp requirement for the next level
+  def xp_check(self):
+    if self.xp[0] > self.xp[1]:
+      self.level += 1
+      self.xp[0] -= self.xp[1]
+      # XP to next level follows a logarithmic curve base 10 multiplier.
+      self.xp[1] = round((math.log(self.level, 10) + 1) * self.xp[1], 0)
+      color_print(Fore.GREEN, "You've leveled up!")
+      self.level_up()
 
-	def show_inventory(self):
-		print("INVENTORY:")
-		inventory_names = []
-		# make list of names from inventory items
-		for item in self.inventory:
-			inventory_names.append(item.name)
-		print(inventory_names)
-		inventory_action = input_handler(
-			1, 4, "What would you like to do? \n"
-			"-1- Re-roll modifier\n"
-			"-2- Equip item\n"
-			"-3- Inspect item\n"
-			"-4- Exit\n")
-		if inventory_action != 4:
-			select_item = input_handler(1, len(self.inventory), "Which item?") - 1
-		if inventory_action == 1:
-			if self.inventory[select_item].isEquipped:
-				color_print(Fore.RED, "Can't re-roll while equipped")
-			else:
-				self.inventory[select_item].roll_mod()
-				self.inventory[select_item].stats()
-				color_print(Fore.GREEN, "Mod re-rolled\n")
-		elif inventory_action == 2:
-			self.equip_item(self.inventory[select_item])
-		elif inventory_action == 3:
-			self.inventory[select_item].stats()
-		elif inventory_action == 4:
-			system("clear")
+  def show_inventory(self):
+    print("INVENTORY:")
+    # make list of names from inventory items before printing
+    for item in self.inventory:
+      print(f"({self.inventory.index(item) + 1}) {item.name}")
+    inventory_action = input_handler(
+      1, 4, "What would you like to do? \n"
+      "-1- Re-roll modifier\n"
+      "-2- Equip item\n"
+      "-3- Inspect item\n"
+      "-4- Exit\n")
+    # if action isn't exit, select item
+    if inventory_action != 4:
+      select_item = input_handler(1, len(self.inventory), "Which item?") - 1
+    if inventory_action == 1:
+      if self.inventory[select_item].isEquipped:
+        color_print(Fore.RED, "Can't re-roll while equipped")
+      else:
+        self.inventory[select_item].roll_mod()
+        self.inventory[select_item].stats()
+        color_print(Fore.GREEN, "Mod re-rolled\n")
+    elif inventory_action == 2:
+      self.equip_item(self.inventory[select_item])
+    elif inventory_action == 3:
+      self.inventory[select_item].stats()
+    elif inventory_action == 4:
+      system("clear")
 
-	# check if an item is equipped before adding/removing stats
-	def equip_item(self, item):
-		if item.isEquipped:
-			multiplier = -1
-			print(f"You have unequipped {item.name}")
-		else:
-			multiplier = 1
-			print(f"You have equipped {item.name}")
-		self.health[0] += item.health * multiplier
-		self.health[1] += item.health * multiplier
-		self.mana[0] += item.mana * multiplier
-		self.mana[1] += item.mana * multiplier
-		self.attack += item.attack * multiplier
-		self.defence += item.defence * multiplier
-		item.isEquipped = not item.isEquipped
+  # check if an item is equipped before adding/removing stats
+  def equip_item(self, item):
+    if item.isEquipped:
+      multiplier = -1
+      print(f"You have unequipped {item.name}")
+    else:
+      multiplier = 1
+      print(f"You have equipped {item.name}")
+    self.health[0] += item.health * multiplier
+    self.health[1] += item.health * multiplier
+    self.mana[0] += item.mana * multiplier
+    self.mana[1] += item.mana * multiplier
+    self.attack += item.attack * multiplier
+    self.defence += item.defence * multiplier
+    item.isEquipped = not item.isEquipped
 
 
 class Monster:
@@ -273,10 +270,7 @@ class Monster:
 
 
 player = Character()
-
-
 player.inventory.append(Equipment("Iron", "Sword"))
-
 player.stats()
 
 while player.health[0] > 0:
@@ -319,5 +313,7 @@ while player.health[0] > 0:
 			color_print(Fore.RED, "that's not a valid number!")
 	except:
 		color_print(Fore.RED, "that's not a valid number!")
+
+
 
 print("Game Over")

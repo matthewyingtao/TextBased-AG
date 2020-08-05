94acbea3f03e9a66d88d043b036c30971a5ddc40
Matthew Tao <s134584@gapps.dilworth.school.nz>
2020-08-05 04:41:37 +0000

    Move stats to class

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from implicits import implicits
 from monsters import gargantuan_monsters, huge_monsters, large_monsters, medium_monsters, small_monsters, tiny_monsters
 from colorama import Fore, Style
 
+"""idk know if it's you ash but DONT PRESS AUTO FORMAT CAUSE IT SCREWS EVERYTHING UP PLEASE"""
+
 tier_limits = {
 	"gargantuan_monsters": [50, 10, 10],
 	"huge_monsters": [30, 9, 6],
@@ -99,6 +101,7 @@ def attack_check():
 def battle(combat_monster):
 	print(f"You are fighting a {combat_monster.name}")
 	while combat_monster.health > 0 and player.health[0] > 0:
+		combat_monster.stats()
 		damage_calc(player, attacks_list[attack_check()], combat_monster)
 		if combat_monster.health > 0:
 			damage_calc(combat_monster, attacks_list[0], player)
@@ -114,6 +117,13 @@ def battle(combat_monster):
 def get_base_stats(item_type):
 	base_stats = equips.get(item_type)
 	return base_stats
+
+class Stats:
+	def __init__(self, **stats):
+		self.stats = []
+		for name, value in stats.items():
+			self.stats.append(f"{name}: {value}")
+		color_print(Fore.GREEN, "STATS:", *self.stats)
 
 
 class Equipment:
@@ -150,117 +160,96 @@ class Equipment:
 		self.defence += self.mod_effect[3]
 
 	def stats(self):
-		self.attributes = vars(self)
-		self.attributes_keys = list(self.attributes.keys())
-		self.attributes_keys.pop()
-		print(f"{Fore.GREEN}STATS:")
-		for attribute in self.attributes_keys:
-			print(
-				f"{(attribute.replace('_', ' ').upper())}: {self.attributes.get(attribute)}"
-			)
-		del self.attributes
-		del self.attributes_keys
-		print(Style.RESET_ALL)
+		self.print_stats = Stats(Name=self.name, Health=self.health, Mana=self.mana, Attack=self.attack, Defence=self.defence, Mod=self.mod_effect, Equipped=self.isEquipped)
 
 
 class Character:
-  def __init__(self, name):
-    self.name = name
-    self.health = [10, 10]
-    self.mana = [5, 5]
-    self.attack = 3
-    self.defence = 0
-    self.xp = [0, 50]
-    self.level = 1
-    self.inventory = []
+	def __init__(self, name):
+		self.name = name
+		self.health = [10, 10]
+		self.mana = [5, 5]
+		self.attack = 3
+		self.defence = 0
+		self.xp = [0, 50]
+		self.level = 1
+		self.inventory = []
 
-  def stats(self):
-    self.attributes = vars(self)
-    self.attributes_keys = list(self.attributes.keys())
-    self.attributes_keys.pop()
-    self.attributes_keys.pop()
-    print(f"{Fore.GREEN}STATS:")
-    for attribute in self.attributes_keys:
-      print(
-        f"{(attribute.replace('_', ' ').upper())}: {self.attributes.get(attribute)}"
-      )
-    del self.attributes
-    del self.attributes_keys
-    print(Style.RESET_ALL)
+	def stats(self):
+		self.print_stats = Stats(Name=self.name, Health=self.health, Mana=self.mana, Attack=self.attack, Defence=self.defence, XP=self.xp, Level=self.level)
 
-  # allocation of stat points
-  def level_up(self):
-    points = 3
-    while points > 0:
-      allocate = input_handler(
-        1, 4, "Allocate points:\n"
-        "-1- HEALTH\n"
-        "-2- MANA\n"
-        "-3- ATTACK\n"
-        "-4- DEFENCE\n")
-      if allocate == 1:
-        self.health[1] += 1
-      elif allocate == 2:
-        self.mana[1] += 1
-      elif allocate == 3:
-        self.attack += 1
-      elif allocate == 4:
-        self.defence += 1
-      points -= 1
+	# allocation of stat points
+	def level_up(self):
+		points = 3
+		while points > 0:
+			allocate = input_handler(
+			1, 4, "Allocate points:\n"
+			"-1- HEALTH\n"
+			"-2- MANA\n"
+			"-3- ATTACK\n"
+			"-4- DEFENCE\n")
+			if allocate == 1:
+					self.health[1] += 1
+			elif allocate == 2:
+				self.mana[1] += 1
+			elif allocate == 3:
+				self.attack += 1
+			elif allocate == 4:
+				self.defence += 1
+			points -= 1
 
-  # check xp after battle, and increases the xp requirement for the next level
-  def xp_check(self):
-    if self.xp[0] > self.xp[1]:
-      self.level += 1
-      self.xp[0] -= self.xp[1]
-      # XP to next level follows a logarithmic curve base 10 multiplier.
-      self.xp[1] = round((math.log(self.level, 10) + 1) * self.xp[1], 0)
-      color_print(Fore.GREEN, "You've leveled up!")
-      self.level_up()
+	# check xp after battle, and increases the xp requirement for the next level
+	def xp_check(self):
+		if self.xp[0] > self.xp[1]:
+			self.level += 1
+			self.xp[0] -= self.xp[1]
+			# XP to next level follows a logarithmic curve base 10 multiplier.
+			self.xp[1] = round((math.log(self.level, 10) + 1) * self.xp[1], 0)
+			color_print(Fore.GREEN, "You've leveled up!")
+			self.level_up()
 
-  def show_inventory(self):
-    print("INVENTORY:")
-    # make list of names from inventory items before printing
-    for item in self.inventory:
-      print(f"({self.inventory.index(item) + 1}) {item.name}")
-    inventory_action = input_handler(
-      1, 4, "What would you like to do? \n"
-      "-1- Re-roll modifier\n"
-      "-2- Equip item\n"
-      "-3- Inspect item\n"
-      "-4- Exit\n")
-    # if action isn't exit, select item
-    if inventory_action != 4:
-      select_item = input_handler(1, len(self.inventory), "Which item?") - 1
-    if inventory_action == 1:
-      if self.inventory[select_item].isEquipped:
-        color_print(Fore.RED, "Can't re-roll while equipped")
-      else:
-        self.inventory[select_item].roll_mod()
-        self.inventory[select_item].stats()
-        color_print(Fore.GREEN, "Mod re-rolled\n")
-    elif inventory_action == 2:
-      self.equip_item(self.inventory[select_item])
-    elif inventory_action == 3:
-      self.inventory[select_item].stats()
-    elif inventory_action == 4:
-      system("clear")
+	def show_inventory(self):
+		print("INVENTORY:")
+		# make list of names from inventory items before printing
+		for item in self.inventory:
+			print(f"({self.inventory.index(item) + 1}) {item.name}")
+		inventory_action = input_handler(
+		1, 4, "What would you like to do? \n"
+		"-1- Re-roll modifier\n"
+		"-2- Equip item\n"
+		"-3- Inspect item\n"
+		"-4- Exit\n")
+		# if action isn't exit, select item
+		if inventory_action != 4:
+			select_item = input_handler(1, len(self.inventory), "Which item?") - 1
+		if inventory_action == 1:
+			if self.inventory[select_item].isEquipped:
+				color_print(Fore.RED, "Can't re-roll while equipped")
+			else:
+				self.inventory[select_item].roll_mod()
+				self.inventory[select_item].stats()
+				color_print(Fore.GREEN, "Mod re-rolled\n")
+		elif inventory_action == 2:
+			self.equip_item(self.inventory[select_item])
+		elif inventory_action == 3:
+			self.inventory[select_item].stats()
+		elif inventory_action == 4:
+			system("clear")
 
-  # check if an item is equipped before adding/removing stats
-  def equip_item(self, item):
-    if item.isEquipped:
-      multiplier = -1
-      print(f"You have unequipped {item.name}")
-    else:
-      multiplier = 1
-      print(f"You have equipped {item.name}")
-    self.health[0] += item.health * multiplier
-    self.health[1] += item.health * multiplier
-    self.mana[0] += item.mana * multiplier
-    self.mana[1] += item.mana * multiplier
-    self.attack += item.attack * multiplier
-    self.defence += item.defence * multiplier
-    item.isEquipped = not item.isEquipped
+	# check if an item is equipped before adding/removing stats
+	def equip_item(self, item):
+		if item.isEquipped:
+			multiplier = -1
+			print(f"You have unequipped {item.name}")
+		else:
+			multiplier = 1
+			print(f"You have equipped {item.name}")
+		self.health[0] += item.health * multiplier
+		self.health[1] += item.health * multiplier
+		self.mana[0] += item.mana * multiplier
+		self.mana[1] += item.mana * multiplier
+		self.attack += item.attack * multiplier
+		self.defence += item.defence * multiplier
+		item.isEquipped = not item.isEquipped
 
 
 class Monster:
@@ -270,6 +259,12 @@ class Monster:
 		self.attack = tier_limits.get(tier)[1]
 		self.defence = tier_limits.get(tier)[2]
 		self.xp = random.randint(20, 40)
+		
+
+	def stats(self):
+		self.print_stats = Stats(Name=self.name, Health=self.health, Attack=self.attack, Defence=self.defence)
+
+monster = Monster(monster_tiers[1], monster_tiers_names[1])
 
 name = input("what is your name? \n")
 system("clear")

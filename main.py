47c17fa216799bc9fbf00a5596f283b1c8c139c7
Matthew Tao <s134584@gapps.dilworth.school.nz>
2020-08-08 04:37:42 +0000

    Add emoji to difficulty select

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import random
 import os
+import random
+import emoji
 from math import ceil, log
+
+from colorama import Fore
+
 from implicits import implicits
 from monsters import gargantuan_monsters, huge_monsters, large_monsters, medium_monsters, small_monsters, tiny_monsters
-from colorama import Fore
-import emoji
 
 tier_limits = {
     "gargantuan_monsters": [50, 10, 10, 800, 1000],
@@ -81,6 +83,7 @@ def loot():
     else:
         print("No loot dropped")
 
+
 def damage_calc(attacker, move, defender):
     # get random float between 0-1 and check for miss
     miss = random.random()
@@ -127,7 +130,6 @@ def battle(combat_monster):
         color_print(Fore.GREEN, f"You have defeated {combat_monster.name}")
         player.xp[0] += combat_monster.xp
         player.xp_check()
-        player.coin += random.randint(25,50) #monster_tier x1.25
         loot()
     elif player.health[0] < 0:
         color_print(Fore.RED, f"{combat_monster.name} has defeated you")
@@ -198,13 +200,12 @@ class Character:
         self.defence = 0
         self.xp = [0, 50]
         self.level = 1
-        self.coin = 0
         self.inventory = []
         self.equipment = []
 
     def stats(self):
         print_stats(Name=self.name, Health=self.health, Mana=self.mana, Attack=self.attack,
-                    Defence=self.defence, XP=self.xp, Level=self.level, Coin=self.coin) #Coin=self.coin
+                    Defence=self.defence, XP=self.xp, Level=self.level)
 
     # allocation of stat points
     def level_up(self):
@@ -308,29 +309,20 @@ player.stats()
 while True:
     while player.health[0] > 0:
         choice = input_handler(
-            0, 5, "What would you like to do? \n"
+            0, 3, "What would you like to do? \n"
                   "-1- Adventure \n"
                   "-2- Go to an inn \n"
                   "-3- View inventory\n"
-                  "-4- Merchant\n"
-                  "-5- Settings\n")
+                  "-4- Shop\n")
         if choice == 1:
             difficulty = input_handler(
-                0, 7, "Which dungeon?\n"
-                      "\n"
-                      #emoji.emojize(":bug:" ":shamrock:"   ":evergreen_tree:" ":evergreen_tree:" ":evergreen_tree:\n")
-                      " -1-       -2-        -3-\n"
-                      "Plains - Forest - Magic Forest\n"
-                      "\n"
-                      #emoji.emojize(":crab:"":palm_tree:" ":spiral_shell:" ":volcano:\n")
-                      "    -4-    -5-   -6-\n"
-                      "   Caves - Bay - Hell\n"
-                      "\n"
-                      "           -7-\n"
-                      "          Space\n")
-
-#possible emoji :mountain: :snow-capped_mountain: :camping: :national park: :desert island: :sun: :crescent_moon: :rainbow: :fire: :comet: :water_wave: :sparkles: :dollar_banknote: :label: :chart_increasing: :crossed_swords: :shield: :warning: :shopping_cart: :white_medium_square: :black_medium_square: :green_square: :clinking_beer_mugs: :beer_mug: :dizzy:
-
+                0, 6, "Which dungeon?\n",
+                emoji.emojize("-1- The Plains :bug:\n"
+                              "-2- The Forest :evergreen_tree:\n"
+                              "-3- The Caves :gem_stone:\n"
+                              "-4- The Magic Forest :crystal_ball:\n"
+                              "-5- The Bay :water_wave:\n"
+                              "-6- Hell :fire:\n"))
             cls()
             monster = Monster(monster_tiers[difficulty - 1], monster_tiers_names[difficulty - 1])
             battle(monster)
@@ -342,8 +334,6 @@ while True:
         elif choice == 3:
             player.show_inventory()
         elif choice == 4:
-            print("Not added yet!")
-        elif choice == 5:
             print("Not added yet!")
 
     restart = input_handler(
